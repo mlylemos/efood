57ee4fd2001e5d3204b1bd193004a53095bb8878
Emily Lemos <157964741+mlylemos@users.noreply.github.com>
2025-07-09 19:03:15 -0300

    Update styles.ts

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,8 +9,7 @@ export const HeaderBar = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 171px;
-  position: relative; // Não precisa ser absolute
+  position: relative;
   width: 100%;
   top: 0;
   left: 0;
