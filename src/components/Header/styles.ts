42eaa14f54d94406b8b202807df1735b3cf7bce0
Emily Lemos <157964741+mlylemos@users.noreply.github.com>
2025-07-09 19:06:44 -0300

    Update styles.ts

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -34,7 +34,7 @@ export const Cart = styled.div`
   font-size: 18px;
   font-weight: bold;
   font-family: 'Roboto', sans-serif;
-  margin-left: 10px;
+  margin-left: 8px;
   margin-right: 100px;
   white-space: nowrap;
 `
